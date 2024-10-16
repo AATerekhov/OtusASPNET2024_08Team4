63eb704f5bdc2da936332f634f7683e41d0a5019
VladimirFKulagin <kulaginvlad1991@gmail.com>
2024-10-16 22:53:08 +0300

    Correct model & correct record to class in Dto

--- SantaClaus'sBag/SantasBag.Core/Domain/Administration/RewardTemplate.cs
+++ SantaClaus'sBag/SantasBag.Core/Domain/Administration/RewardTemplate.cs
@@ -11,9 +11,10 @@ namespace SantasBag.Core.Domain.Administration
         public required string Name { get; set; }
         public string? Description { get; set; }
         public string? Image {  get; set; }
-        public int Cost {  get; set; }
+        public int Cost {  get; set; } = 10;
         public bool LimitedCount {  get; set; } = false;
         public int? TotalCount { get; set;}
+        public required Guid RoomId { get; set; }
 
     }
 }
--- SantaClaus'sBag/SantasBag.Core/Domain/Contracts/RewardedDto.cs
+++ SantaClaus'sBag/SantasBag.Core/Domain/Contracts/RewardedDto.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SantasBag.Core.Domain.Contracts
+﻿namespace SantasBag.Core.Domain.Contracts
 {
-    public record RewardedDto(string Name, string? Description, int Cost, Guid RoomId);
+    public class RewardedDto
+    {
+        public required string Name;
+        public string? Description;
+        public int Cost;
+        public Guid RoomId;
+    }
 }
