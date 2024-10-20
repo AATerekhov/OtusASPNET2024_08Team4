fffbdb7e502910f1da433f6bea1fe1c6267e38da
AATerekhov <A.A.Terekhov@mail.ru>
2024-10-21 00:48:13 +0300

    #24 issues  - I am continuing to work on the EF configuration

---
+++ BookOfHabits/BookOfHabitsMicroservice.Infrastructure.EntityFramework/Configurations/PersonConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using BookOfHabitsMicroservice.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookOfHabitsMicroservice.Infrastructure.EntityFramework.Configurations
+{
+    public class PersonConfiguration : IEntityTypeConfiguration<Person>
+    {
+        public void Configure(EntityTypeBuilder<Person> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
