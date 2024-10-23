0bbf86657d3cf3e3656cca45adea010cfe7809c6
VladimirFKulagin <kulaginvlad1991@gmail.com>
2024-10-24 00:46:33 +0300

    Correct layers architecture

---
+++ SantasBag/SantasBag/Contracts/RewardTemplate.cs
@@ -0,0 +1,15 @@
+﻿using SantasBag.Core.Models;
+
+namespace SantasBag.WebHost.Contracts;
+
+public class RewardTemplate: BaseEntity 
+{
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public string? Image {  get; set; }
+    public int Cost {  get; set; } = 10;
+    public bool LimitedCount {  get; set; } = false;
+    public int? TotalCount { get; set;}
+    public required Guid RoomId { get; set; }
+
+}
