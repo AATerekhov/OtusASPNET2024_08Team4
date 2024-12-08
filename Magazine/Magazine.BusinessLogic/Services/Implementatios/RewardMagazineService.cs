0df703b57a46a9a35c920f98752e8cd6e5400ca7
ainur <ainur@AINURPC>
2024-12-08 22:40:30 +0300

    Issue 68, 69

---
+++ Magazine/Magazine.BusinessLogic/Services/Implementatios/RewardMagazineService.cs
@@ -0,0 +1,89 @@
+﻿using AutoMapper;
+using Magazine.BusinessLogic.Models.RewardMagazine;
+using Magazine.Core.Domain.Abstractions;
+using Magazine.Core.Domain.Magazines;
+using Magazine.Core.Exceptions;
+using Magazine.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magazine.BusinessLogic.Services.Implementatios
+{
+    public class RewardMagazineService : BaseService, IRewardMagazineService
+    {
+        private readonly IMapper _mapper;
+        private readonly IRewardMagazineRepository _magazineRepository;
+
+        public RewardMagazineService(
+           IMapper mapper,
+           IRewardMagazineRepository magazineRepository)
+        {
+            _mapper = mapper;
+            _magazineRepository = magazineRepository;
+        }
+
+        public async Task<RewardMagazine> CreateAsync(CreateOrEditRewardMagazineDto createOrEditMagazineDto, CancellationToken cancellationToken)
+        {
+            var magazine        = _mapper.Map<CreateOrEditRewardMagazineDto, RewardMagazine>(createOrEditMagazineDto);
+            var createdMagazine = await _magazineRepository.AddAsync(magazine, cancellationToken);
+
+            await _magazineRepository.SaveChangesAsync(cancellationToken);
+
+            return createdMagazine;
+        }
+
+        public async Task DeleteAsync(Guid id, CancellationToken cancellationToken)
+        {
+            var magazine = await _magazineRepository.GetByIdAsync(id, cancellationToken)
+                       ?? throw new NotFoundException(FormatFullNotFoundErrorMessage(id, nameof(RewardMagazine)));
+
+
+            _magazineRepository.Delete(magazine);
+
+            await _magazineRepository.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task<List<RewardMagazine>> GetAllAsync(CancellationToken cancellationToken)
+        {
+            return await _magazineRepository.GetAllAsync(cancellationToken, true);
+        }
+
+        public async Task<List<RewardMagazine>> GetAllByMagazineOwnerIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            return await _magazineRepository.GetAllByMagazineOwnerIdAsync(id, cancellationToken);
+        }
+
+        public async Task<RewardMagazine> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            return await _magazineRepository.GetByIdAsync(id, cancellationToken, nameof(RewardMagazine.Lines))
+                     ?? throw new NotFoundException(FormatFullNotFoundErrorMessage(id, nameof(RewardMagazine)));
+        }
+
+        public async Task<ICollection<RewardMagazine>> GetPagedAsync(RewardMagazineFilterDto filterDto, CancellationToken cancellationToken)
+        {
+            return await _magazineRepository.GetPagedAsync(
+               _mapper.Map<RewardMagazineFilterDto, RewardMagazineFilterModel>(filterDto),
+               cancellationToken
+           );
+        }
+
+        public async Task<RewardMagazine> UpdateAsync(Guid id, CreateOrEditRewardMagazineDto createOrEditMagazineDto, CancellationToken cancellationToken)
+        {
+            var magazine = await _magazineRepository.GetByIdAsync(id, cancellationToken)
+                     ?? throw new NotFoundException(FormatFullNotFoundErrorMessage(id, nameof(RewardMagazine)));
+
+            magazine.Description     = !string.IsNullOrWhiteSpace(createOrEditMagazineDto.Description) ? createOrEditMagazineDto.Description : magazine.Description;
+            magazine.MagazineOwnerId = createOrEditMagazineDto.MagazineOwnerId != Guid.Empty ? createOrEditMagazineDto.MagazineOwnerId : magazine.MagazineOwnerId;
+            magazine.RoomId          = createOrEditMagazineDto.RoomId != Guid.Empty ? createOrEditMagazineDto.RoomId : magazine.RoomId;
+            magazine.UserId          = createOrEditMagazineDto.UserId != Guid.Empty ? createOrEditMagazineDto.UserId : magazine.UserId;
+
+            _magazineRepository.Update(magazine);
+            await _magazineRepository.SaveChangesAsync(cancellationToken);
+
+            return magazine;
+        }
+    }
+}
---
+++ Magazine/Magazine/Cache/KeyForCache.cs
@@ -0,0 +1,8 @@
+﻿namespace MagazineHost.Cache
+{
+    public static class KeyForCache
+    {
+        public static string RewardMagazineKey(string methodName) => $"RewardMagazine{methodName}";
+        public static string RewardMagazineLineKey(string methodName) => $"RewardMagazineLine{methodName}";
+    }
+}
